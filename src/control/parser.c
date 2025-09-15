8c3f536d22de7ae2cfd0d04e0583603c0da4a83a
tusharsingh09 <tushar8bit@gmail.com>
2025-09-15 23:53:55 +0530

    clean up

--- src/control/parser.c
+++ src/control/parser.c
@@ -18,6 +18,10 @@ void exec_B    (uint32_t instruction, RegFile* registers);
 void exec_J    (uint32_t instruction, RegFile* registers);
 void exec_U    (uint32_t instruction, RegFile* registers);
 
+inline void nop(void){
+    return;
+}
+
 void parser_set_mask(Parser* parser){
     uint8_t opcode = parser->instrucion&0x3f;
     uint8_t* mask = &parser->instruction_mask;
@@ -62,82 +66,32 @@ void parser_exec(Parser* parser, RegFile* regfile){
             exec_IALU(parser->instrucion, regfile);
             break;
         case I_LOAD:
+            exec_ILOAD(parser->instrucion, regfile);
             break;
         case S:
+            exec_S(parser->instrucion, regfile);
             break;
         case B:
+            exec_B(parser->instrucion, regfile);
             break;
         case J:
+            exec_J(parser->instrucion, regfile);
             break;
         case U:
+            exec_U(parser->instrucion, regfile);
             break;
         default:
-            break;
+            nop();
     }
 }
 
 void exec_RType(uint32_t instruction, RegFile* registers){
     uint8_t funct7 = instruction & 0xfe000000;
-    uint8_t funct3 = instruction & 0x7000;
-    uint8_t rd = instruction & 0xf80;
-    uint8_t rs1 = registers->registers[instruction&0xf8000];
-    uint8_t rs2 = registers->registers[instruction&0x1f0000];    
-
-    switch(funct3){
-        case 0x0:
-            switch(funct7){
-                case 0x0: // ADD
-                    registers->registers[rd] = rs1 + rs2;
-                    break;
-                case 0x2: // SUB
-                    registers->registers[rd] = rs1-rs2;
-                    break;
-            }
-            break;
-
-        case 0x4: // XOR
-            registers->registers[rd] = rs1^rs2;
-            break;
-
-        case 0x6: // OR
-            registers->registers[rd] = rs1|rs2;
-            break;
-
-        case 0x7: // AND
-            registers->registers[rd] = rs1&rs2;
-            break;
-
-        case 0x1: // SLL
-            registers->registers[rd] = rs1 << rs2;
-            break;
-
-        case 0x5:
-            switch(funct7){
-                case 0x0: // SRL
-                    registers->registers[rd] = rs1 >> rs2;
-                    break;
-
-                case 0x2: // SRA
-                    registers->registers[rd] = (int32_t)rs1 >> (int32_t)rs2;
-                    break;
-            }
-            break;
-
-        case 0x2: // SLT
-            registers->registers[rd] = rs1 < rs2;
-            break;
-        
-        case 0x3: // SLTU
-            registers->registers[rd] = (uint32_t)rs1 < (uint32_t)rs2;
-            break;
-        
-        default: // NOP
-            registers->registers[rd] = registers->registers[rd];
-    }
+    uint8_t rs2    = instruction & 0xf8;
 }
 
 void exec_IALU (uint32_t instruction, RegFile* registers){
-
+    
 }
 
 void exec_ILOAD(uint32_t instruction, RegFile* registers){
